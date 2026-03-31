28975010258ba430c3f4e691b53d9337630973f4
jsegelh <jsegelh@siue.edu>
2026-03-31 02:51:49 -0500

    Added code for movemet of enemies 1, 2, and 3

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -20,7 +20,7 @@ public class Enemy : MonoBehaviour{
         }
     }
 
-    private BoundsCheck bndCheck;
+    protected BoundsCheck bndCheck;
 
     void Awake(){
         bndCheck = GetComponent<BoundsCheck>();
