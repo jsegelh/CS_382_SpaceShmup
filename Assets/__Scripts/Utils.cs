7d498c112cfaa99c06348b43b90f739173b71799
jsegelh <jsegelh@siue.edu>
2026-04-01 14:05:52 -0500

    Made enemies blink red when damaged

--- Assets/__Scripts/Utils.cs
+++ Assets/__Scripts/Utils.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Utils : MonoBehaviour{
 
-    //== Bezier Curves ==========================================================\\
+//== Bezier Curves ==========================================================\\
     /// <summary>
     /// While most Bezier curves are 3 or 4 points, it is possible to have 
     /// any number of points using this recursive function.
@@ -28,5 +28,22 @@ public class Utils : MonoBehaviour{
         }
         // When complete, vArr[0,0] holds the final interpolated value
         return vArr[0,0];
+    }
+
+//== Materials Functions ====================================================\\
+
+    /// <summary>
+    /// Returns a list of all Materials on this GameObject and its children
+    /// </summary>
+    /// <param name="go">The GameObject on which to search for Renderers</param>
+    static public Material[] GetAllMaterials(GameObject go){
+        Renderer[] rends = go.GetComponentsInChildren<Renderer>();
+
+        Material[] mats = new Material[rends.Length];
+        for (int i = 0; i < rends.Length; i++){
+            mats[i] = rends[i].material;
+        }
+
+        return mats;
     }
 }
